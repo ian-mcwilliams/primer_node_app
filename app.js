902564421df6dbeef350aadb7862dd3d46228216
ian-mcwilliams <ian.mcwilliams@f3mmedia.com>
2019-05-19 12:04:29 +0100

    add configuration using config package

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-
+const config = require('config');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const Joi = require('joi');
@@ -15,6 +15,11 @@ const courses = [
 
 app.use(express.json());
 app.use(helmet());
+
+// Configuration
+console.log(`Application Name: ${config.get('name')}`);
+console.log(`Mail Server: ${config.get('mail.host')}`);
+console.log(`Mail Password: ${config.get('mail.password')}`);
 
 if (app.get('env') === 'development') {
   app.use(morgan('tiny'));

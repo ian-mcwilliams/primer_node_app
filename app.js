aa8caab28aa9f11616e3b90d78f738f89f26d081
ian-mcwilliams <ian.mcwilliams@f3mmedia.com>
2019-05-19 13:35:36 +0100

    move courses routes into courses module

--- app.js
+++ app.js
@@ -2,21 +2,16 @@ const debug = require('debug')('app:startup');
 const config = require('config');
 const morgan = require('morgan');
 const helmet = require('helmet');
-const Joi = require('joi');
 const logger = require('./logger');
 const express = require('express');
 const app = express();
+const courses = require('./routes/courses');
 const port = process.env.PORT || 3000;
-
-const courses = [
-  { id: 1, name: 'course1' },
-  { id: 2, name: 'course2' },
-  { id: 3, name: 'course3' }
-];
 
 app.use(express.json());
 // use helmet security features for express app
 app.use(helmet());
+app.use('/api/courses/', courses);
 
 // Configuration
 debug(`Application Name: ${config.get('name')}`);
@@ -30,62 +25,8 @@ if (app.get('env') === 'development') {
 
 app.use(logger);
 
-const validateCourse = course => {
-  const schema = {
-    name: Joi.string().min(3).required()
-  };
-  return Joi.validate(course, schema);
-};
-
-const getCourse = courseId => {
-  return courses.find(c => c.id === parseInt(courseId));
-};
-
 app.get('/', (req, res) => {
   res.send('Hello World!!!');
-});
-
-app.get('/api/courses', (req, res) => {
-  res.send(courses);
-});
-
-app.post('/api/courses', (req, res) => {
-  const { error } = validateCourse(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
-
-  const course = {
-    id: courses.length + 1,
-    name: req.body.name
-  };
-  courses.push(course);
-  res.send(course);
-});
-
-app.get('/api/courses/:id', (req, res) => {
-  const course = getCourse(req.params.id);
-  if (!course) return res.status(404).send(`No course was found with id ${req.params.id}`);
-  res.send(course);
-});
-
-app.put('/api/courses/:id', (req, res) => {
-  const course = getCourse(req.params.id);
-  if (!course) return res.status(404).send(`No course was found with id ${req.params.id}`);
-
-  const { error } = validateCourse(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
-
-  course.name = req.body.name;
-  res.send(course);
-});
-
-app.delete('/api/courses/:id', (req, res) => {
-  const course = getCourse(req.params.id);
-  if (!course) return res.status(404).send(`No course was found with id ${req.params.id}`);
-
-  const index = courses.indexOf(course);
-  courses.splice(index, 1);
-
-  res.send(course);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}...`));

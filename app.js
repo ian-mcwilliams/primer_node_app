47952e892b53b95da3961c9252bd3e4f2f786871
ian-mcwilliams <ian.mcwilliams@f3mmedia.com>
2019-06-02 19:45:54 +0100

    move config output to startup/config and use winston for logging

--- app.js
+++ app.js
@@ -15,13 +15,6 @@ const port = process.env.PORT || 3000;
 // use helmet security features for express app
 app.use(helmet());
 
-// Configuration
-debug(`Application Name: ${config.get('name')}`);
-debug(`Mail Server: ${config.get('mail.host')}`);
-// if the following line errors it may be because the password environment variable needs to be added
-// in the terminal window, eg: export app_password=1234
-debug(`Mail Password: ${config.get('mail.password')}`);
-
 if (app.get('env') === 'development') {
   app.use(morgan('tiny'));
   debug('Morgan enabled...')
--- startup/config.js
+++ startup/config.js
@@ -1,4 +1,5 @@
 const config = require('config');
+const winston = require('winston');
 
 module.exports = function() {
   // ensure jwtPrivateKey is set, eg: export coursely_jwtPrivateKey=jwtPrivateKey
@@ -6,4 +7,8 @@ module.exports = function() {
 
   // ensure mail.password is set, eg: export coursely_mailPassword=mailPassword
   if (!config.get('mail.password')) throw new Error('mail.password is not defined.');
+
+  winston.info(`Application Name: ${config.get('name')}`);
+  winston.info(`Mail Server: ${config.get('mail.host')}`);
+  winston.info(`Mail Password: ${config.get('mail.password')}`);
 };

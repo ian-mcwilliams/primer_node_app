a70e6e933591e2009155ff72f4c2e91761b8dd28
ian-mcwilliams <ian.mcwilliams@f3mmedia.com>
2019-06-05 23:19:52 +0100

    add test db

--- startup/db.js
+++ startup/db.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 const winston = require('winston');
+const config = require('config');
 
 module.exports = async function() {
-  await mongoose.connect('mongodb://localhost/coursely');
-  winston.info('Connected to MongoDB...');
+  const db = config.get('db');
+  await mongoose.connect(db);
+  winston.info(`Connected to ${db}...`);
 };

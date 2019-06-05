28e9801d3cf35f7aa7786e1981d22d67b65a10fe
ian-mcwilliams <ian.mcwilliams@f3mmedia.com>
2019-06-05 23:15:06 +0100

    require winston in app.js

--- app.js
+++ app.js
@@ -1,20 +1,25 @@
 const helmet = require('helmet');
 const morgan = require('morgan');
 const logger = require('./middleware/logger');
+const winston = require('winston');
 const express = require('express');
 const app = express();
+
+winston.info('Starting Coursely...');
+
 require('./startup/logging')();
 require('./startup/routes')(app);
 require('./startup/db')();
 require('./startup/config');
 const port = process.env.PORT || 3000;
 
+
 // use helmet security features for express app
 app.use(helmet());
 
 if (app.get('env') === 'development') {
   app.use(morgan('tiny'));
-  winston.info('Morgan enabled...')
+  winston.info('Morgan enabled...');
 }
 
 app.use(logger);

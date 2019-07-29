222cfa69fccabf7dff7a62cce0b049eac7c6ac1c
ian-mcwilliams <ian.mcwilliams@f3mmedia.com>
2019-07-29 21:40:29 +0100

    add auth tests

---
+++ __tests__/integration/auth.test.js
@@ -0,0 +1,43 @@
+const { User } = require('../../models/user');
+const { Course } = require('../../models/course');
+const request = require('supertest');
+
+let server;
+
+describe('auth middleware', () => {
+  let token;
+
+  beforeEach(() => {
+    server = require('../../app');
+    token = new User().generateAuthToken();
+  });
+
+  afterEach(async () => {
+    await Course.remove({});
+    server.close();
+  });
+
+  const exec = () => {
+    return request(server)
+      .post('/api/courses')
+      .set('x-auth-token', token)
+      .send({name: 'course1'});
+  };
+
+  it('returns 401 if no token is provided', async () => {
+    token = '';
+    const res = await exec();
+    expect(res.status).toBe(401);
+  });
+
+  it('returns 400 if token is invalid', async () => {
+    token = 'a';
+    const res = await exec();
+    expect(res.status).toBe(400);
+  });
+
+  it('returns 200 if token is valid', async () => {
+    const res = await exec();
+    expect(res.status).toBe(200);
+  });
+});
